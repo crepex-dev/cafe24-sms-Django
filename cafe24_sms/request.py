eb00104c8ffcab903b55183898158113bf2ec1e9
hyunwoo.shim <shim.hw0810@gmail.com>
2018-11-16 15:32:51 +0900

    Add result check request class, method

--- cafe24_sms/request.py
+++ cafe24_sms/request.py
@@ -1,4 +1,5 @@
 import functools
+import xml.etree.ElementTree as ET
 
 from django.utils.translation import ugettext_lazy as _
 
@@ -12,8 +13,10 @@ from .settings import module_settings
 class RequestBase:
     SMS_SEND_URL = 'https://sslsms.cafe24.com/sms_sender.php'
     SEND_RESULT_URL = 'http://smsapi.cafe24.com/sms_list.php'
-    SUCCESS_CODE = 'Test Success!' if module_settings.TEST_MODE else 'success'
+    SEND_SUCCESS_CODE = 'Test Success!' if module_settings.TEST_MODE else 'success'
+    CHECK_SUCCESS_CODE = '0000'
     RESERVATION_CODE = 'reserved'
+    TIMEOUT = module_settings.REQUEST_TIMEOUT
 
     def __init__(self, request_data, **kwargs):
         self.request_data = request_data
@@ -49,8 +52,8 @@ class SMSRequest:
                 message=_('Response decode error with %s' % char_set)
             )
 
-        if result_code not in [self.SUCCESS_CODE, self.RESERVATION_CODE] or response.status_code != 200:
-            error_message = get_result_message(result_code, self.request_data.is_lms)
+        if result_code not in [self.SEND_SUCCESS_CODE, self.RESERVATION_CODE] or response.status_code != 200:
+            error_message = get_result_message(result_code)
             raise exceptions.ReceivedErrorResponse(
                 code=result_code,
                 message=error_message
@@ -59,13 +62,49 @@ class SMSRequest:
         return result_code, remaining_count
 
 
-class Request(RequestBase, SMSRequest):
-    """SMS request class. SMSData instance required.\n
-    Init with :class:`cafe24_sms.data.SMSData` instance and use `sms_request` method to try sms message request
-    and get result_code/remaining sms count tuple or exception.\n
+class ResultCheckRequest:
+    def result_check_request(self, url, timeout=None):
+        requests_post = requests.post
+        char_set = module_settings.CHARSET
+
+        if timeout is not None:
+            requests_post = functools.partial(requests_post, timeout=timeout)
+
+        try:
+            response = requests_post(url, data=self.data)
+            xml_tree = ET.fromstring(response.content.decode('utf-8'))
+            result_code = xml_tree.findtext('code', 'UNKNOWN')
+        except requests.exceptions.RequestException:
+            raise exceptions.RequestNotReachable()
+        except UnicodeDecodeError:
+            raise exceptions.SMSModuleException(
+                message=_('Response decode error with %s' % char_set)
+            )
+
+        if result_code != self.CHECK_SUCCESS_CODE or response.status_code != 200:
+            error_message = get_result_message(result_code)
+            raise exceptions.ReceivedErrorResponse(
+                code=result_code,
+                message=error_message
+            )
+
+        return result_code, xml_tree
+
+
+class Request(RequestBase, SMSRequest, ResultCheckRequest):
+    """SMS request class. SMSRequestData instance required.\n
+    Init with :class:`cafe24_sms.data.SMSRequestData` instance and
+    use `sms_request` method to try sms message request\n
+    as a result, get result_code/remaining sms count tuple or exception.\n
+    OR\n
+    Init with :class:`cafe24_sms.data.ResultCheckRequestData` instance and
+    use `result_check` method to try send result request\n
+    as a result, get result_code/result xml tree tuple or exception.\n
     For detail, see also the Cafe24 site: `<https://www.cafe24.com/?controller=myservice_hosting_sms_example>`_.\n
     """
-    timeout = module_settings.REQUEST_TIMEOUT
 
     def send_message(self):
-        return self.sms_request(self.SMS_SEND_URL, self.timeout)
+        return self.sms_request(self.SMS_SEND_URL, self.TIMEOUT)
+
+    def result_check(self):
+        return self.result_check_request(self.SEND_RESULT_URL, self.TIMEOUT)
