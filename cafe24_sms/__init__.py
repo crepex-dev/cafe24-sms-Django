9ad1367cbc421a921b77f97d96c6c5ff34e278ad
hyunwoo.shim <shim.hw0810@gmail.com>
2018-11-16 16:14:37 +0900

    Add result check shortcut function

--- cafe24_sms/__init__.py
+++ cafe24_sms/__init__.py
@@ -5,10 +5,12 @@ from . import exceptions
 from .request import Request
 from .shortcuts import send_message
 from .shortcuts import reserve_message
+from .shortcuts import result_check
 from .result_codes import get_result_message
 
 
 __all__ = (
+    'result_check',
     'get_result_message',
     'reserve_message',
     'send_message',
--- cafe24_sms/shortcuts.py
+++ cafe24_sms/shortcuts.py
@@ -1,3 +1,5 @@
+from .data import ResultCheckRequestData
+from .data import ResultCheckXMLData
 from .data import SMSRequestData
 from .request import Request
 
@@ -51,3 +53,32 @@ def reserve_message(message, receiver, reservation_time,
                           reservation_time=reservation_time, rpt_num=rpt_num, rpt_time=rpt_time)
     request = Request(request_data=data)
     return request.send_message()
+
+
+def result_check(start_date, start_index=0, send_type='all', send_status='S',
+                 receive_num=None, send_num=None, check_range=7):
+    """Shortcut API for :class:`cafe24_sms.data.ResultCheckRequestData`, :class:`cafe24_sms.request.Request`.\n
+    Send check sent sms result request use with :func:`requests.post`.\n
+    For detail, see also the Cafe24 site: `<https://www.cafe24.com/?controller=myservice_hosting_sms_example>`_.\n
+
+    :param datetime.datetime start_date: Check standard date
+    :param int start_index: Search item start index (Optional)
+    :param str send_type: Search param. sent SMS type. Choice in all, general, reserve  (Optional)
+    :param str send_status: Search param. Result status. S(Success), F(Fail) (Optional)
+    :param str receive_num: Search received telephone number separated by '-'. (Optional)
+    :param str send_num: Search sent telephone number separated by '-'. (Optional)
+    :param int check_range: Search range day (Optional)
+
+    :return: str result_code(Received result code), :class:`xml.etree.ElementTree.Element` result xml root element
+
+    :raises: :class:`cafe24_sms.exceptions.RequestNotReachable`.
+    :raises: :class:`cafe24_sms.exceptions.SMSModuleException`.
+    :raises: :class:`cafe24_sms.exceptions.ReceivedErrorResponse`.
+    """
+    data = ResultCheckRequestData(start_date, start_index, send_type, send_status,
+                                  receive_num=receive_num, send_num=send_num, check_range=check_range)
+    request = Request(request_data=data)
+    result_code, root = request.result_check()
+    xml_data = ResultCheckXMLData(code=result_code, root=root)
+
+    return xml_data
