71769df55e17373cb39d14d41c29b079f6fd7ee8
hyunwoo.shim <shim.hw0810@gmail.com>
2018-11-16 21:21:13 +0900

    Modified send test error function param

--- tests/send_test.py
+++ tests/send_test.py
@@ -28,7 +28,7 @@ class SendFunctionsTestCase(TestCase):
     def test_over_length_lms_send_failure(self):
         with self.assertRaisesMessage(
                 cafe24_sms.exceptions.ReceivedErrorResponse,
-                str(cafe24_sms.get_result_message('0004', True))
+                str(cafe24_sms.get_result_message('0004'))
         ):
             result_code, remaining_count = cafe24_sms.send_message(
                 self.max_length_lms + '더',
