30231199865507daa44e09446f8aa935605d6d9b
hyunwoo.shim <shim.hw0810@gmail.com>
2018-11-16 15:20:24 +0900

    Add string to datetime method

--- cafe24_sms/utils.py
+++ cafe24_sms/utils.py
@@ -1,4 +1,6 @@
 from django.utils import timezone
+from django.utils.timezone import is_aware
+from django.utils.timezone import make_aware
 
 import pytz
 
@@ -9,3 +11,11 @@ def get_local_datetime(datetime):
     tz_name = module_settings.TIMEZONE or timezone.get_current_timezone_name()
     tz = pytz.timezone(tz_name)
     return datetime.astimezone(tz)
+
+
+def trans_string_to_datetime(date, date_format):
+    ret = timezone.datetime.strptime(date, date_format)
+    if not is_aware(ret):
+        # Cafe24's result timezone is Asia/Seoul
+        return make_aware(ret).replace(tzinfo=pytz.timezone('Asia/Seoul'))
+    return ret
